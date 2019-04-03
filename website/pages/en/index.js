b6ed4cf6c184894facd9abc891ed8103b56b6b23
Forbes Lindesay <forbes@lindesay.co.uk>
2019-04-03 15:05:07 +0100

    docs: add logo/branding (#11)

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -22,7 +22,7 @@ class HomeSplash extends React.Component {
     const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
 
     const SplashContainer = props => (
-      <div className="homeContainer">
+      <div className="homeContainer" style={{background: '#FA2B3A'}}>
         <div className="homeSplashFade">
           <div className="wrapper homeWrapper">{props.children}</div>
         </div>
@@ -37,37 +37,15 @@ class HomeSplash extends React.Component {
 
     const ProjectTitle = () => (
       <h2 className="projectTitle">
-        {siteConfig.title}
-        <small>{siteConfig.tagline}</small>
+        <img src={`${baseUrl}img/word-mark.svg`} />
+        <small style={{color: 'white'}}>{siteConfig.tagline}</small>
       </h2>
-    );
-
-    const PromoSection = props => (
-      <div className="section promoSection">
-        <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const Button = props => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
-      </div>
     );
 
     return (
       <SplashContainer>
-        <Logo img_src={`${baseUrl}img/docusaurus.svg`} />
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
-          <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
-          </PromoSection>
         </div>
       </SplashContainer>
     );
@@ -83,7 +61,8 @@ class Index extends React.Component {
       <Container
         padding={['bottom', 'top']}
         id={props.id}
-        background={props.background}>
+        background={props.background}
+      >
         <GridBlock
           align="center"
           contents={props.children}
@@ -95,7 +74,8 @@ class Index extends React.Component {
     const FeatureCallout = () => (
       <div
         className="productShowcaseSection paddingBottom"
-        style={{textAlign: 'center'}}>
+        style={{textAlign: 'center'}}
+      >
         <h2>Feature Callout</h2>
         <MarkdownBlock>These are features of this project</MarkdownBlock>
       </div>
@@ -145,16 +125,28 @@ class Index extends React.Component {
       <Block layout="fourColumn">
         {[
           {
-            content: 'This is the content of my feature',
-            image: `${baseUrl}img/docusaurus.svg`,
+            // content: 'This is the content of my feature',
+            image: `${baseUrl}img/postgres.svg`,
             imageAlign: 'top',
-            title: 'Feature One',
+            title: 'Postgres',
           },
           {
-            content: 'The content of my second feature',
-            image: `${baseUrl}img/docusaurus.svg`,
+            // content: 'The content of my second feature',
+            image: `${baseUrl}img/mysql.svg`,
             imageAlign: 'top',
-            title: 'Feature Two',
+            title: 'MySQL',
+          },
+          {
+            // content: 'The content of my second feature',
+            image: `${baseUrl}img/sqlite.svg`,
+            imageAlign: 'top',
+            title: 'SQLite',
+          },
+          {
+            // content: 'The content of my second feature',
+            image: `${baseUrl}img/expo.svg`,
+            imageAlign: 'top',
+            title: 'Expo/WebSQL',
           },
         ]}
       </Block>
@@ -194,10 +186,11 @@ class Index extends React.Component {
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer">
           <Features />
+          {/* 
           <FeatureCallout />
           <LearnHow />
           <TryOut />
-          <Description />
+          <Description /> */}
           <Showcase />
         </div>
       </div>
