1f327195f95d8bfdd6e4ab21876efce37cbbcf97
Forbes Lindesay <forbes@lindesay.co.uk>
2020-05-02 17:25:06 +0100

    chore: update node and yarn (#78)

--- packages/pg-config/src/__tests__/index.test.ts
+++ packages/pg-config/src/__tests__/index.test.ts
@@ -1,64 +1,48 @@
 import {getPgConfigSync, _testReadPgConfigSync} from '..';
 
 test('get root config', () => {
-  expect(getPgConfigSync()).toMatchInlineSnapshot(`
-Object {
-  "connectionStringEnvironmentVariable": "PG_URL",
-  "test": Object {
-    "connectTimeoutSeconds": 20,
-    "containerName": "pg-test",
-    "debug": false,
-    "image": "circleci/postgres:10.6-alpine-ram",
-    "pgDb": "test-db",
-    "pgUser": "test-user",
-  },
-}
-`);
+  expect(getPgConfigSync()).toEqual({
+    connectionStringEnvironmentVariable: 'PG_URL',
+    test: {
+      connectTimeoutSeconds: 20,
+      containerName: 'pg-test',
+      debug: false,
+      image: 'circleci/postgres:10.6-alpine-ram',
+      pgDb: 'test-db',
+      pgUser: 'test-user',
+    },
+  });
 });
 
 test('valid config', () => {
-  expect(_testReadPgConfigSync(__dirname + '/fixtures/empty.json'))
-    .toMatchInlineSnapshot(`
-Object {
-  "connectionStringEnvironmentVariable": "DATABASE_URL",
-  "test": Object {
-    "connectTimeoutSeconds": 20,
-    "containerName": "pg-test",
-    "debug": false,
-    "image": "circleci/postgres:10.6-alpine-ram",
-    "pgDb": "test-db",
-    "pgUser": "test-user",
-  },
-}
-`);
-  expect(_testReadPgConfigSync(__dirname + '/fixtures/override.json'))
-    .toMatchInlineSnapshot(`
-Object {
-  "connectionStringEnvironmentVariable": "PG_CONNECTION",
-  "test": Object {
-    "connectTimeoutSeconds": 20,
-    "containerName": "pg-test",
-    "debug": false,
-    "image": "circleci/postgres:10.6-alpine-ram",
-    "pgDb": "test-db",
-    "pgUser": "test-user",
-  },
-}
-`);
+  expect(_testReadPgConfigSync(__dirname + '/fixtures/empty.json')).toEqual({
+    connectionStringEnvironmentVariable: 'DATABASE_URL',
+    test: {
+      connectTimeoutSeconds: 20,
+      containerName: 'pg-test',
+      debug: false,
+      image: 'circleci/postgres:10.6-alpine-ram',
+      pgDb: 'test-db',
+      pgUser: 'test-user',
+    },
+  });
+  expect(_testReadPgConfigSync(__dirname + '/fixtures/override.json')).toEqual({
+    connectionStringEnvironmentVariable: 'PG_CONNECTION',
+    test: {
+      connectTimeoutSeconds: 20,
+      containerName: 'pg-test',
+      debug: false,
+      image: 'circleci/postgres:10.6-alpine-ram',
+      pgDb: 'test-db',
+      pgUser: 'test-user',
+    },
+  });
 });
 
 test('invalid config', () => {
-  expect(() => _testReadPgConfigSync(__dirname + '/fixtures/invalid.json'))
-    .toThrowErrorMatchingInlineSnapshot(`
-"PgConfig.connectionStringEnvironmentVariable should be string
-
-{ connectionStringEnvironmentVariable: 10,
-  test:
-   { connectTimeoutSeconds: 20,
-     containerName: 'pg-test',
-     debug: false,
-     image: 'circleci/postgres:10.6-alpine-ram',
-     pgDb: 'test-db',
-     pgUser: 'test-user' } }"
-`);
+  expect(() =>
+    _testReadPgConfigSync(__dirname + '/fixtures/invalid.json'),
+  ).toThrowError(
+    /PgConfig.connectionStringEnvironmentVariable should be string/,
+  );
 });
