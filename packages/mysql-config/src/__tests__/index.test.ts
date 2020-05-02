a2511d84aa7e83762dfac22452abe5f58d602ce1
Forbes Lindesay <forbes@lindesay.co.uk>
2020-05-02 17:21:19 +0100

    test: replace some snapshots with non-robken `toEqual` calls

--- packages/mysql-config/src/__tests__/index.test.ts
+++ packages/mysql-config/src/__tests__/index.test.ts
@@ -1,71 +1,53 @@
 import {getMySqlConfigSync, _testReadMySqlConfigSync} from '..';
 
 test('get root config', () => {
-  expect(getMySqlConfigSync()).toMatchInlineSnapshot(`
-Object {
-  "connectionStringEnvironmentVariable": "MYSQL_URL",
-  "test": Object {
-    "connectTimeoutSeconds": 20,
-    "containerName": "mysql-test",
-    "debug": false,
-    "image": "mysql:5.7.24",
-    "mySqlDb": "test-db",
-    "mySqlPassword": "password",
-    "mySqlUser": "test-user",
-  },
-}
-`);
+  expect(getMySqlConfigSync()).toEqual({
+    connectionStringEnvironmentVariable: 'MYSQL_URL',
+    test: {
+      connectTimeoutSeconds: 20,
+      containerName: 'mysql-test',
+      debug: false,
+      image: 'mysql:5.7.24',
+      mySqlDb: 'test-db',
+      mySqlPassword: 'password',
+      mySqlUser: 'test-user',
+    },
+  });
 });
 
 test('valid config', () => {
-  expect(_testReadMySqlConfigSync(__dirname + '/fixtures/empty.json'))
-    .toMatchInlineSnapshot(`
-Object {
-  "connectionStringEnvironmentVariable": "DATABASE_URL",
-  "test": Object {
-    "connectTimeoutSeconds": 20,
-    "containerName": "mysql-test",
-    "debug": false,
-    "image": "mysql:5.7.24",
-    "mySqlDb": "test-db",
-    "mySqlPassword": "password",
-    "mySqlUser": "test-user",
-  },
-}
-`);
-  expect(_testReadMySqlConfigSync(__dirname + '/fixtures/override.json'))
-    .toMatchInlineSnapshot(`
-Object {
-  "connectionStringEnvironmentVariable": "PG_CONNECTION",
-  "test": Object {
-    "connectTimeoutSeconds": 20,
-    "containerName": "mysql-test",
-    "debug": false,
-    "image": "mysql:5.7.24",
-    "mySqlDb": "test-db",
-    "mySqlPassword": "password",
-    "mySqlUser": "test-user",
-  },
-}
-`);
+  expect(_testReadMySqlConfigSync(__dirname + '/fixtures/empty.json')).toEqual({
+    connectionStringEnvironmentVariable: 'DATABASE_URL',
+    test: {
+      connectTimeoutSeconds: 20,
+      containerName: 'mysql-test',
+      debug: false,
+      image: 'mysql:5.7.24',
+      mySqlDb: 'test-db',
+      mySqlPassword: 'password',
+      mySqlUser: 'test-user',
+    },
+  });
+  expect(
+    _testReadMySqlConfigSync(__dirname + '/fixtures/override.json'),
+  ).toMatchInlineSnapshot({
+    connectionStringEnvironmentVariable: 'PG_CONNECTION',
+    test: {
+      connectTimeoutSeconds: 20,
+      containerName: 'mysql-test',
+      debug: false,
+      image: 'mysql:5.7.24',
+      mySqlDb: 'test-db',
+      mySqlPassword: 'password',
+      mySqlUser: 'test-user',
+    },
+  });
 });
 
 test('invalid config', () => {
-  expect(() => _testReadMySqlConfigSync(__dirname + '/fixtures/invalid.json'))
-    .toThrowErrorMatchingInlineSnapshot(`
-    "MySqlConfig.connectionStringEnvironmentVariable should be string
-
-    {
-      connectionStringEnvironmentVariable: 10,
-      test: {
-        connectTimeoutSeconds: 20,
-        containerName: 'mysql-test',
-        debug: false,
-        image: 'mysql:5.7.24',
-        mySqlDb: 'test-db',
-        mySqlPassword: 'password',
-        mySqlUser: 'test-user'
-      }
-    }"
-  `);
+  expect(() =>
+    _testReadMySqlConfigSync(__dirname + '/fixtures/invalid.json'),
+  ).toThrowError(
+    /MySqlConfig.connectionStringEnvironmentVariable should be string/,
+  );
 });
