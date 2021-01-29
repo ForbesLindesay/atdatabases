cfa1e1a6cdc3a13f0d911d0710a45187947cf5af
Forbes Lindesay <forbes@lindesay.co.uk>
2021-01-29 14:34:53 +0000

    test: update mysql versions

--- .github/workflows-src/test.ts
+++ .github/workflows-src/test.ts
@@ -167,11 +167,7 @@ export default createWorkflow(({setWorkflowName, addTrigger, addJob}) => {
     const {node, mysql} = setBuildMatrix(
       {
         node: ['12.x', '14.x'],
-        mysql: [
-          '5.6.50',
-          '5.7.32',
-          // '8.0.22',
-        ],
+        mysql: ['5.6.51', '5.7.33', '8.0.23'],
       },
       {failFast: false},
     );
