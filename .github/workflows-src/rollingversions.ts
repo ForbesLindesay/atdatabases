5ba572abf6d8049fea811ea2b9c95527ff727cc3
Forbes Lindesay <forbes@lindesay.co.uk>
2026-03-10 14:45:36 +0000

    feat: use GitHub actions to publish releases (#345)

---
+++ .github/workflows-src/rollingversions.ts
@@ -0,0 +1,45 @@
+import createWorkflow from 'github-actions-workflow-builder';
+import {secrets} from 'github-actions-workflow-builder/context';
+import {setup} from './test';
+
+export default createWorkflow(
+  ({setWorkflowName, addTrigger, addJob, setPermissions, whenTrigger}) => {
+    setWorkflowName('Release');
+
+    addTrigger('repository_dispatch', {
+      types: ['rollingversions_publish_approved'],
+    });
+    addTrigger('push', {branches: ['master']});
+
+    setPermissions({
+      'id-token': 'write', // Required for OIDC to publish to NPM
+      contents: 'write', // To create GitHub releases
+    });
+
+    whenTrigger('push', () => {
+      addJob('publish_canary', ({add, run}) => {
+        add(setup());
+        run('yarn build');
+        run(
+          'npx rollingversions publish --canary $GITHUB_RUN_NUMBER --allow-any-branch',
+          {
+            env: {
+              GITHUB_TOKEN: secrets.GITHUB_TOKEN,
+            },
+          },
+        );
+      });
+    });
+    whenTrigger('repository_dispatch', () => {
+      addJob('publish', ({add, run}) => {
+        add(setup());
+        run('yarn build');
+        run('npx rollingversions publish', {
+          env: {
+            GITHUB_TOKEN: secrets.GITHUB_TOKEN,
+          },
+        });
+      });
+    });
+  },
+);
